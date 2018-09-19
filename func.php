d2098b609ee38d884e2e62489cdc00ad08763c4c
Charlie Jarl <charlie.jarl@gmail.com>
2018-09-19 10:25:30 +0200

    func

--- func.php
+++ func.php
@@ -4,4 +4,29 @@
     require_once "//lang//"+$strLang+".php";
     $debug="//lang//"+$strLang+".php";
 
+    class Database
+    {
+        /**
+         * Database constructor.
+         * @param $host
+         * @param $user
+         * @param $pass
+         * @param $dbs
+         */
+        public function __construct($host, $user, $pass, $dbs)
+        {
+                $this->mysqli = mysqli_connect($host, $user, $pass, $dbs);
+        }
+    
+        /**
+         * @param $query
+         * @return bool|mysqli_result
+         */
+
+        public function($query)
+        {
+            return $this->mysqli->query($query);
+        }
+
+    }
 ?>
