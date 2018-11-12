0acc3e1c0db63c449f32f13a670f493fbe8659f0
Charlie Jarl <charlie.jarl@gmail.com>
2018-11-12 14:07:24 +0100

    20181112 update

--- index.php
+++ index.php
@@ -45,31 +45,59 @@
   <span class="closeX" onclick="this.parentElement.style.display='none'">X</span>
   <h3><?=$strInvestigation;?></h3>
     <form action="action.php?frm=utr" name="frmUtr" id="frmUtr">
-      <input type="text" class="frmTextInput" id="">
-      <input type="text" class="frmTextInput">
-      <input type="text" class="frmTextInput">
-      <input type="text" class="frmTextInput">
-      <input type="text" class="frmTextInput">
-      <input type="text" class="frmTextInput">
+      <textarea class="frmTextInput" name="client_what_happened" id="client_what_happened" rows=3><?=$client_what_happened;?></textarea>
+      <textarea class="frmTextInput" name="background" id="background" rows=3><?=$background;?></textarea>
+      <textarea class="frmTextInput" name="current_situation" id="current_situation" rows=3><?=$current_situation;?></textarea>
+      <textarea class="frmTextInput" name="contractors_comment" id="contractors_comment" rows=3><?=$contractors_comment;?></textarea>
+      <textarea class="frmTextInput" name="union_comment" id="union_comment" rows=3><?=$union_comment;?></textarea>
+      <textarea class="frmTextInput" name="other_comment" id="other_comment" rows=3><?=$other_comment;?></textarea>
+      <textarea class="frmTextInput" name="student_perspective" id="student_perspective" rows=3><?=$student_perspective;?></textarea>
+      <input class="frmTextInput" id="local_agreements" name="local_agreements" type="text" placeholder="<?=$local_agreements;?>">
+      <input class="frmTextInput" id="timeframe" name="timeframe" type="text" placeholder="<?=$timeframe;?>">
+      <input class="frmTextInput" id="policys" name="policys" type="text" placeholder="<?=$policys;?>">
+      <input class="frmTextInput" id="practice" name="practice" type="text" placeholder="<?=$practice?>">
+      <input class="frmTextInput" id="mediator_contacted_at" name="mediator_contacted_at" type="text" placeholder="<?=$mediator_contacted_at;?>">
+      <input class="frmTextInput" id="are_there_regulations_in_laws_or_contracts" name="are_there_regulations_in_laws_or_contracts" type="text" placeholder="<?=$are_there_regulations_in_laws_or_contracts;?>">
     </form>
 </div>
 
 <div id="three" class="tabcontent">
   <span class="closeX" onclick="this.parentElement.style.display='none'">X</span>
   <h3><?=$strStrategy;?></h3>
-  <p>Paris is the capital of France.</p> 
+  <form action="action.php?frm=strat" name="frmStrat" id="frmStrat">
+      <textarea class="frmTextInput" id="what_do_client_want" name="what_do_client_want" rows=3><?=$what_do_client_want;?></textarea> 
+      <textarea class="frmTextInput" id="union_practice" name="union_practice" rows=3><?=$union_practice;?></textarea>
+      <textarea class="frmTextInput" id="what_do_contractor_want" name="what_do_contractor_want" rows=3><?=$what_do_contractor_want;?></textarea>
+      <input class="frmTextInput" id="who_can_decide" name="who_can_decide" type="text" placeholder="<?=$who_can_decide;?>">
+      <textarea class="frmTextInput" id="considerations" name="considerations" rows=3><?=$considerations;?></textarea>
+    </form>
 </div>
 
 <div id="four" class="tabcontent">
   <span class="closeX" onclick="this.parentElement.style.display='none'">X</span>
   <h3><?=$strAction;?></h3>
-  <p>Tokyo is the capital of Japan.</p>
+  <form action="action.php?frm=action" name="frmAction" id="frmAction">
+      <textarea class="frmTextInput" id="what_do_we_do_now" name="what_do_we_do_now" rows=3><?=$what_do_we_do_now;?></textarea>
+      <input class="frmTextInput" id="who_does_what_and_when" name="who_does_what_and_when" type="text" placeholder="<?=$who_does_what_and_when;?>">
+      <input class="frmTextInput" id="who_do_we_contact" name="who_do_we_contact" type="text" placeholder="<?=$who_do_we_contact;?>">
+      <textarea class="frmTextInput" id="negotiation_request" name="negotiation_request" rows=3><?=$negotiation_request;?></textarea>
+      <textarea class="frmTextInput" id="arguments" name="arguments" rows=3><?=$arguments;?></textarea>
+      <textarea class="frmTextInput" id="recruitment_steps" name="recruitment_steps" rows=3><?=$recruitment_steps;?></textarea>
+    </form>
 </div>  
 
 <div id="five" class="tabcontent">
   <span class="closeX" onclick="this.parentElement.style.display='none'">X</span>
   <h3><?=$strResult;?></h3>
-  <p>Tokyo is the capital of Japan.</p>
+  <form action="action.php?frm=res" name="frmRes" id="frmRes">
+      <textarea class="frmTextInput" id="own_reflection" name="own_reflection" rows=3><?=$own_reflection;?></textarea>
+      <textarea class="frmTextInput" id="feedback_to_client" name="feedback_to_client" rows=3><?=$feedback_to_client;?></textarea>
+      <textarea class="frmTextInput" id="result" name="result" rows=3><?=$result;?></textarea>
+      <textarea class="frmTextInput" id="analyses" name="analyses" rows=3><?=$analyses;?></textarea>
+      <textarea class="frmTextInput" id="recruitment_communication" name="recruitment_communication" rows=3><?=$recruitment_communication;?></textarea>
+      <textarea class="frmTextInput" id="feedback_to_mediator" name="feedback_to_mediator" rows=3><?=$feedback_to_mediator;?></textarea>
+      <textarea class="frmTextInput" id="case_knowledge" name="case_knowledge" rows=3><?=$case_knowledge;?></textarea>
+    </form>
 </div>  
     </div>
 <script>
